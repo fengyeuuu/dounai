ee9c98a1e3f673f4404f0913832d03354d5562f8
Fengye <88529388+fengyeuuu@users.noreply.github.com>
2024-07-19 09:09:55 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -9,9 +9,9 @@ from bs4 import BeautifulSoup
 # 从 Secrets 中获取敏感信息
 email = os.environ.get('EMAIL')
 password = os.environ.get('PASSWORD')
-email_password = os.environ.get('ADDRESS_PASSWORD')
-from_address = os.environ.get('FROM_ADDRESS')
-receiver_address = os.environ.get('RECEIVER_ADDRESS')
+# email_password = os.environ.get('ADDRESS_PASSWORD')
+# from_address = os.environ.get('FROM_ADDRESS')
+# receiver_address = os.environ.get('RECEIVER_ADDRESS')
 
 data = {
     'email': email,
@@ -50,22 +50,22 @@ s = soup.find_all('div',class_='card-inner margin-bottom-no')
 
 tmp = re.findall(r'<code>(.*?)</code>', str(s[0]))
 
-meg += '等级0过期时间：' + tmp[6] + '\n'
-meg += '签到：' + checkin.text.encode().decode("unicode_escape")
+# meg += '等级0过期时间：' + tmp[6] + '\n'
+# meg += '签到：' + checkin.text.encode().decode("unicode_escape")
 
 
-# 邮件基本信息
-smtp_server = "smtp.qq.com"
-smtp_port = 465
+# # 邮件基本信息
+# smtp_server = "smtp.qq.com"
+# smtp_port = 465
 
-# 邮件构造
-message = MIMEText(meg, "plain", "utf-8")
-message["From"] = Header(from_address)
-message["To"] = Header(receiver_address, "utf-8")
-message["Subject"] = Header("来自豆豆豆奶的签到信息", "utf-8")
+# # 邮件构造
+# message = MIMEText(meg, "plain", "utf-8")
+# message["From"] = Header(from_address)
+# message["To"] = Header(receiver_address, "utf-8")
+# message["Subject"] = Header("来自豆豆豆奶的签到信息", "utf-8")
 
-# 发送邮件
-smtpObject = smtplib.SMTP_SSL(smtp_server, smtp_port)
-smtpObject.login(from_address, email_password)
-smtpObject.sendmail(from_address, [receiver_address], message.as_string())
-smtpObject.quit()
+# # 发送邮件
+# smtpObject = smtplib.SMTP_SSL(smtp_server, smtp_port)
+# smtpObject.login(from_address, email_password)
+# smtpObject.sendmail(from_address, [receiver_address], message.as_string())
+# smtpObject.quit()
